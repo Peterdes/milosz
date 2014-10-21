a1958c94cf1486a8ef5abfb59ec3bf39bb10f070
Piotr Suwara <peter_de_sowaro@o2.pl>
2014-10-21 23:14:35 +0200

    Cosmetic changes

--- rozwiniete.h
+++ rozwiniete.h
@@ -25,45 +25,47 @@ class Sklepikarz : public Rozwiniete
 {
 public:
 	Sklepikarz(queue<string> *const, RandEngine&);
-    virtual ~Sklepikarz();
+	virtual ~Sklepikarz();
 	Sklepikarz(const Sklepikarz&) = delete;
 	Sklepikarz& operator=(const Sklepikarz&) = delete;
 
-    const string& przedstaw() const;
-    bool ruszSie();
-    void rozmawiaj(Stworzenie *); //sprzedaj
+	const string& przedstaw() const;
+	bool ruszSie();
+	void rozmawiaj(Stworzenie *); //sprzedaj
+
 protected:
-    bool ruszSie(Kierunek);
+	bool ruszSie(Kierunek);
+	
 private:
-    static const string wyglad;
-    bool _maZbroje;
-    float _zbroja;
-    bool _maBron;
-    float _bron;
-    bool _maPrezent;
+	static const string wyglad;
+	bool _maZbroje;
+	float _zbroja;
+	bool _maBron;
+	float _bron;
+	bool _maPrezent;
 	static const float _dostawy;	//prawdopodobieństwo dostawy w danej turze
 
 	RandEngine& _gen;
 	static std::uniform_real_distribution<float> dist;
 
-    void init();
+	void init();
 };
 
 class Znachorka : public Rozwiniete
 {
 public:
 	Znachorka(queue<string> *const, RandEngine&);
-    virtual ~Znachorka();
+	virtual ~Znachorka();
 	Znachorka(const Znachorka&) = delete;
 	Znachorka& operator=(const Znachorka&) = delete;
 
-    const string& przedstaw() const;
-    bool ruszSie();
-    void rozmawiaj(Stworzenie *); //spróbuj uleczyć
+	const string& przedstaw() const;
+	bool ruszSie();
+	void rozmawiaj(Stworzenie *); //spróbuj uleczyć
 protected:
-    bool ruszSie(Kierunek);
+	bool ruszSie(Kierunek);
 private:
-    static const string wyglad;
+	static const string wyglad;
 };
 
 
@@ -71,19 +73,19 @@ class Bard : public Rozwiniete
 {
 public:
 	Bard(const Gra *, Pole *, queue<string> *const, RandEngine&);
-    virtual ~Bard();
+	virtual ~Bard();
 	Bard(const Bard&) = delete;
 	Bard& operator=(const Bard&) = delete;
 
-    const string& przedstaw() const;
-    bool ruszSie();
-    void rozmawiaj(Stworzenie *); //śpiewaj
+	const string& przedstaw() const;
+	bool ruszSie();
+	void rozmawiaj(Stworzenie *); //śpiewaj
 protected:
-    bool ruszSie(Kierunek);
+	bool ruszSie(Kierunek);
 private:
-    static const string wyglad;
-    Pole * _skarb;
-    const Gra * _gra;
+	static const string wyglad;
+	Pole * _skarb;
+	const Gra * _gra;
 };
 
 
@@ -91,29 +93,29 @@ class Poszukiwacz : public Rozwiniete
 {
 public:
 	Poszukiwacz(float zbroja, float bron, queue<string> *const komunikaty,
-		 int ruch, int sila, int maxZdrowie, int zdrowie);
+		int ruch, int sila, int maxZdrowie, int zdrowie);
 	Poszukiwacz(queue<string> *const komunikaty,
-		 int ruch, int sila, int maxZdrowie, int zdrowie, RandEngine& gen);
+		int ruch, int sila, int maxZdrowie, int zdrowie, RandEngine& gen);
 	Poszukiwacz(queue<string> *const, RandEngine&);
-    virtual ~Poszukiwacz();
+	virtual ~Poszukiwacz();
 	Poszukiwacz(const Poszukiwacz&) = delete;
 	Poszukiwacz& operator=(const Poszukiwacz&) = delete;
 
-    virtual const string& przedstaw() const;
-    virtual bool ruszSie();
-    float zbroja() const;
-    float bron() const;
-    void rozmawiaj(Stworzenie *); //ignoruj
+	virtual const string& przedstaw() const;
+	virtual bool ruszSie();
+	float zbroja() const;
+	float bron() const;
+	void rozmawiaj(Stworzenie *); //ignoruj
 	bool wezZbroje(float);  //chętnie
-    bool wezBron(float);  //jasne
+	bool wezBron(float);  //jasne
 
 protected:
-    virtual bool ruszSie(Kierunek);
+	virtual bool ruszSie(Kierunek);
 
 private:
-    float _zbroja;
-    float _bron;
-    static const string wyglad;
+	float _zbroja;
+	float _bron;
+	static const string wyglad;
 };
 
 
